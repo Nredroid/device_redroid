ea6ce0ef532902cfc4cd7c72c8d7b19b2502a7e2
ColdWindScholar <3590361911@qq.com>
2026-07-14 15:54:52 +0800

    DMA Heap's enabled in most of linux releases

--- c2/c2.mk
+++ c2/c2.mk
@@ -1,4 +1,3 @@
 PRODUCT_COPY_FILES += \
     device/redroid/c2/redroid.c2.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/redroid.c2.rc \
-    device/redroid/c2/redroid.c2.sh:$(TARGET_COPY_OUT_VENDOR)/bin/redroid.c2.sh \
     device/redroid/c2/mediaswcodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaswcodec.policy \
