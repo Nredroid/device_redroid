c1d44026b06d5a907154fb364c45ec4fddba47e8
ColdWindScholar <3590361911@qq.com>
2026-07-16 06:46:09 +0800

    build mesa3d from source

--- redroid.mk
+++ redroid.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
     android.hardware.security.keymint-service android.hidl.allocator@1.0-service
 
 PRODUCT_COPY_FILES += \
+    external/mesa3d/src/util/00-mesa-defaults.conf:$(TARGET_COPY_OUT_VENDOR)/etc/drirc \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
