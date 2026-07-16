dbbbdfcd476b5b09b0b388a34ffbbc8920655043
ColdWindScholar <3590361911@qq.com>
2026-07-17 05:00:34 +0800

    merge prebuilts to device

--- redroid.mk
+++ redroid.mk
@@ -202,4 +202,27 @@ $(call inherit-product-if-exists, product.mk)
 
 $(call inherit-product, vendor/redroid/vendor.mk)
 
-$(call inherit-product, device/redroid-prebuilts/prebuilts.mk)
+PRODUCT_PACKAGES += \
+    amdgpu.ids.redroid \
+    vulkan.broadcom \
+    vulkan.freedreno \
+    vulkan.radeon \
+    vulkan.nouveau \
+    gralloc.cros \
+    gralloc.gbm \
+    uinputd \
+    vncserver \
+    hwcomposer.redroid \
+    libdrm_amdgpu \
+    libdrm_radeon \
+    libdrm_nouveau \
+    libdrm_freedreno \
+    libdrm_etnaviv \
+    libdrm_intel \
+    libglapi \
+    libevdev \
+    libigdgmm_android
+
+# vaapi
+PRODUCT_PACKAGES += avcenc h264encode hevcencode jpegenc vp8enc vp9enc vainfo
+
