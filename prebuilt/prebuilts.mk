c5380e20dcd8deb417484bcabd269be8fbf86d81
ColdWindScholar <3590361911@qq.com>
2026-07-17 19:27:47 +0800

    build hwcomposer from source

--- prebuilt/prebuilts.mk
+++ prebuilt/prebuilts.mk
@@ -1,6 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-define amdgpu-gpu-ids
 include $$(CLEAR_VARS)
 LOCAL_MODULE := amdgpu.ids.redroid
 LOCAL_MODULE_CLASS := ETC
@@ -9,8 +8,7 @@ LOCAL_SRC_FILES := prebuilts/share/libdrm/amdgpu.ids
 LOCAL_MODULE_RELATIVE_PATH := hwdata
 LOCAL_PROPRIETARY_MODULE := true
 include $$(BUILD_PREBUILT)
-endef
-define va
+
 include $$(CLEAR_VARS)
 LOCAL_MODULE := libva-drm.so
 LOCAL_INSTALLED_MODULE_STEM := libva-drm.so
@@ -26,8 +24,7 @@ LOCAL_MULTILIB := both
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
-endef
-define redroid-audio
+
 include $$(CLEAR_VARS)
 LOCAL_MODULE := audio.primary.redroid
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -43,21 +40,15 @@ LOCAL_MULTILIB := both
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_CHECK_ELF_FILES := false
 include $$(BUILD_PREBUILT)
-endef
-define uinputd-binary
-include $$(CLEAR_VARS)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := uinputd
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES_$$(TARGET_ARCH) := prebuilts/$$(TARGET_ARCH)/bin/uinputd
+LOCAL_SRC_FILES_$(TARGET_ARCH) := prebuilts/$(TARGET_ARCH)/bin/uinputd
 #LOCAL_STRIP_MODULE := false
 LOCAL_MULTILIB := first
 LOCAL_MODULE_TAGS := optional
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_CHECK_ELF_FILES := false
 LOCAL_INIT_RC := prebuilts/share/uinputd/uinputd.rc
-include $$(BUILD_PREBUILT)
-endef
-$(eval $(call uinputd-binary))
-$(eval $(call amdgpu-gpu-ids))
-$(eval $(call va))
-$(eval $(call redroid-audio))
+include $(BUILD_PREBUILT)
